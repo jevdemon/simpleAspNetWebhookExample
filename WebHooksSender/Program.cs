a87d340839fdf3725e1d6d1fba8d589b671200d2
jevdemon <jevdemon@REDLECTROID>
2017-07-28 11:21:31 -0700

    Tweaked code to make it easier to demo

--- WebHooksSender/Program.cs
+++ WebHooksSender/Program.cs
@@ -1,15 +1,17 @@
-﻿// 2 libs required for Webhooks
+﻿// 2 libs required for custom Webhooks
 using Microsoft.AspNet.WebHooks;
 using Microsoft.AspNet.WebHooks.Diagnostics;
 
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;            // added so we can determine where the tracelog is located 
+using System.Reflection;    // added so we can determine where the tracelog is located 
 
 namespace WebHooksSender
 {
     /// <summary>
-    ///  This is a simple hack to show the use of ASP.NET webhooks.
+    ///  This is a simple hack to show the use of ASP.NET webhooks. There is ZERO exception handling because this is only a demo. 
     ///  In this example the "subscriber" (Receiver) and "publisher" (Sender) are in the same code.
     ///  These would normally be in two different services but I wanted to keep this simple.
     ///      
@@ -24,13 +26,18 @@ namespace WebHooksSender
         /// appropriate callback.
         private static IWebHookManager whManager;
         private static IWebHookStore whStore;
-        
-        /// Variable below is the URL for the callback. 
-        /// Go to request bin (http://requestb.in) and update the url below.
-        private static string myRequestBin = "http://requestb.in/1hkv2g11";
-        
+
+        private static string myRequestBin = "";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hit ENTER to create a RequestBin for your callback url");
+            Console.ReadLine();
+            System.Diagnostics.Process.Start("http://requestb.in");
+
+            Console.WriteLine("\nPaste the bin url for your RequestBin below:");
+            myRequestBin = Console.ReadLine();
+
             whStore = new MemoryWebHookStore();
 
             /// A WebHookManager is used to send a webhook request.  
@@ -38,48 +45,56 @@ namespace WebHooksSender
             /// WebHookManager also uses an ILogger-type object as a diagnostics logger. 
             whManager = new WebHookManager(whStore, new TraceLogger());
 
-            Console.WriteLine("Registering a Subscriber with WebHookManager");
+            Console.WriteLine("\n\nRegistering a Subscriber with WebHookManager");
             registerWebhook();
-            Console.WriteLine("\nGo check out your RequestBin at " + myRequestBin + "\n\nHit ENTER to fire your webhook");
+            Console.WriteLine("\nHit ENTER to fire your webhook");
             Console.ReadLine();
 
-            Console.WriteLine("Triggering an event to fire the webhook (normally done by an external service)");
             fireWebhook().Wait();
 
-            Console.WriteLine("\nGo refresh your RequestBin page at " + myRequestBin + "\n\nHit ENTER to exit");
+            Console.WriteLine("\nTracelog is at " + new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName + "\\trace.log");
+
+            Console.WriteLine("Hit ENTER to view the notification in RequestBin");
             Console.ReadLine();
+            System.Diagnostics.Process.Start(myRequestBin + "?inspect");
         }
 
         private static void registerWebhook()
         {
             var webhook = new WebHook();
 
-            Console.WriteLine("Subscriber 'user1' will subscribe to 'event1'");
+            Console.WriteLine("\tSubscriber 'user1' will subscribe to 'event1'");
 
             // Configure the webhook request (subscription)
             webhook.Filters.Add("event1");
             webhook.Properties.Add("FirstName", "John");
             webhook.Properties.Add("LastName", "Evdemon");
+
             // webhook secret is normally not hard-coded (obviously)
             webhook.Secret = "PSBuMnbzZqVir4OnN4DE10IqB7HXfQ9l2";
 
             // Identify the callback URL. 
             webhook.WebHookUri = myRequestBin;
-            
+
             // Register subscription with callback URL to the webhook store
             whStore.InsertWebHookAsync("user1", webhook);
+
+            Console.WriteLine("\tWebhook registered");
+
         }
 
         private static async Task fireWebhook()
         {
             // create a simple notification that indicates an event occurred
             // this would normally occur in another service
-            var notifications = new List<NotificationDictionary> { new NotificationDictionary("event1", new { Value = "Response from Webhook", Message = "Your Event occurred"}) };
+            var notifications = new List<NotificationDictionary> { new NotificationDictionary("event1", new { Value = "Response from Webhook", Message = "Your Event occurred" }) };
 
             // simulate a different event and try to fire it for the receiver 
             // the receiver shouldn't get this notification because it didn't register interest in this event
             notifications.Add(new NotificationDictionary("BogusEvent", new { Value = "Response from Webhook", Message = "Your bogus event occurred" }));
-            
+
+            Console.WriteLine("Firing webhook (normally done by an external service)...");
+
             // fire the webhook with multiple notifications (invoke the callback URL)
             var x = await whManager.NotifyAsync("user1", notifications);
 
